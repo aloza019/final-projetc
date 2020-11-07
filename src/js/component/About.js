ada821588de6d3ad3c3f3bc901956e2f14ccca9e
DianaGuendi <dianaguendica@gmail.com>
2020-11-07 17:05:56 +0000

    probando

--- src/js/component/About.js
+++ src/js/component/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export function About() {
 	return (
 		<div>
-			<h1> This is about pageeeee </h1>
+			<h1> This is about pageeeeiie </h1>
 			<p> New View</p>
 		</div>
 	);
